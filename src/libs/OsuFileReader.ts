ae6ba157a071941363ccfa57cfefacf2ee6214b2
Marius Schmitt <schmittma@elektronikschule.de>
2022-09-29 01:37:19 +0200

    Make OsuFileReader download missing files

--- src/libs/OsuFileReader.ts
+++ src/libs/OsuFileReader.ts
@@ -1,14 +1,35 @@
-import { existsSync } from "fs"
+import { existsSync, writeFileSync } from "fs"
+import axios from "axios";
 
 export class OsuFileReader {
 
-  getOsuFilePathFromId(id: number): string {
+  async getOsuFilePathFromId(id: number): Promise<string> {
     let path = process.cwd() + '/maps/' + id + '.osu';
+
     if(existsSync(path)) {
       return path;
     }
     else {
-      return '';
+      if((await this.downloadOsuFileToFS(id, path)) == true) {
+        return path;
+      }
+      else
+      {
+        return '';
+      }
     }
+  }
+
+  private async downloadOsuFileToFS(id: number, path: string): Promise<boolean> {
+    let success = false;
+
+    const osu_file = await (await axios.get(`https://osu.ppy.sh/osu/${id}`)).data;
+
+    if (osu_file != '') {
+      writeFileSync(path, osu_file);
+      success = true;
+    }
+
+    return success;
   }
 }
